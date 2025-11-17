c39c65f625575edff2df64f948d6b04c664e085f
ByggL <samuel.leobon@gmail.com>
2025-11-17 15:24:26 +0100

    feat: Login endpoints implementation

---
+++ types/api_types.ts
@@ -0,0 +1,6 @@
+export type LoginResponse = {
+  token: string;
+  admin: boolean;
+};
+
+export type ExtendSessionResponse = LoginResponse;
---
+++ utils/api.ts
@@ -0,0 +1,90 @@
+import { ExtendSessionResponse, LoginResponse } from "@/types/api_types";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+
+const storeJwt = async (jwtString: string) => {
+  try {
+    await AsyncStorage.setItem("jwt_token", jwtString);
+  } catch (e) {
+    throw new Error("Failed to store JWT token");
+  }
+};
+
+const getJwt = async () => {
+  try {
+    const value = await AsyncStorage.getItem("jwt_token");
+    if (value !== null) {
+      return value;
+    }
+  } catch (e) {
+    throw new Error("Failed to retrieve JWT token");
+  }
+};
+
+module.exports = {
+  ///////////////////////////////////////
+  //////////// AUTH REQUESTS ////////////
+  ///////////////////////////////////////
+
+  login: async (username: string, password: string): Promise<LoginResponse> => {
+    let response = await axios.post(
+      "https://edu.tardigrade.land/msg/login",
+      {
+        username: username,
+        password: password,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (response.status == 403) {
+      throw new Error("Login failed, invalid credentials");
+    } else if (response.status !== 200) {
+      throw new Error("Login failed");
+    }
+
+    if (response.status === 200) {
+      console.log("Login successful");
+    }
+
+    let data: LoginResponse = response.data;
+
+    await storeJwt(data.token);
+
+    return data;
+  },
+
+  extendSession: async (): Promise<ExtendSessionResponse> => {
+    let jwtToken = await getJwt();
+
+    let response = await axios.post("https://edu.tardigrade.land/msg/protected/extend_session", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    });
+
+    if (response.status == 401) {
+      throw new Error("Session extension failed, invalid token");
+    } else if (response.status !== 200) {
+      throw new Error("Session extension failed");
+    }
+
+    if (response.status === 200) {
+      console.log("Session extension successful");
+    }
+
+    let data: ExtendSessionResponse = response.data;
+
+    return data;
+  },
+
+  ///////////////////////////////////////
+
+  ///////////////////////////////////////
+  //////////// USER REQUESTS ////////////
+  ///////////////////////////////////////
+};
