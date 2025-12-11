414634c87e42de7b4bdae6569922b1c5198f4676
ByggL <samuel.leobon@gmail.com>
2025-12-11 16:00:47 +0100

    feat: created channel page branch

--- app/(tabs)/channelPage.tsx
+++ app/(tabs)/channelPage.tsx
@@ -1,7 +1,21 @@
 import { StyleSheet, Text, View } from "react-native";
 
+interface ChannelPageProps {
+    id: number;
+  name: string;
+  img: string;
+  creator: string;
+  theme: {
+    primary_color: string;
+    primary_color_dark: string;
+    accent_color: string;
+    text_color: string;
+    accent_text_color: string;
+  };
+  users: string[];
+}
 
-export default function CanalPage(){
+export default function ChannelPage(){
     const hasError : boolean = false;
      
     return (
