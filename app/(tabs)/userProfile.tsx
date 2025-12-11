b0f3d11616601fefe41b9f9055880a957ea243a5
EliotLouys <eliot.louys@grenoble-inp.org>
2025-12-11 18:25:28 +0100

    Start of implementation of login logic

--- app/(tabs)/userProfile.tsx
+++ app/(tabs)/userProfile.tsx
@@ -1,12 +1,30 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function ConnectionPage(){
-    const hasError : boolean = false;
-     
-    return (
-        <View >
-            <Text>User profile page</Text>
-            
-        </View>
-    )
+export default function ConnectionPage() {
+  const hasError: boolean = false;
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titles}>User profile page</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titles: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  textInputs: {
+    color: "rgba(0, 0, 0, 1)",
+  },
+  errorMessage: {
+    color: "red",
+    fontSize: 10,
+  },
+});
